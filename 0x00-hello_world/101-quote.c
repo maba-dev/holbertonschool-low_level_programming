c1cd6bce3f3f20e3d564dabf565d4467de9c242f
maba-dev <cherif.ba@hotmail.fr>
2021-02-03 21:07:29 +0000

    using ft_putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+/**
+ * main - Main function
+ * Return: After printing, return 0
+*/
+
+void	ft_putchar(char c)
+
+{
+	write(1, &c, 1);
+}
+
+int	main(main)
+
+{
+	char c[] = "\"and that piece of art is useful\" - Dora Korpar, 2014-10-19"
+	ft_putchar(c)
+	return (0);
+}
+
