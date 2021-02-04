73024b2759009c36882dd7dc2734a8708186eef2
maba-dev <cherif.ba@hotmail.fr>
2021-02-04 12:39:30 +0000

    the correct space

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,24 +3,23 @@
 #include <stdio.h>
 
 /**
+* main - entry poin
 * more headers goes there
 * betty style doc for function main goes there
 * Return: Return description
 */
 
 int	main(void)
-/* main - main description */
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	if (n > 0)
 		printf("%d is positive\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
 	else
-		printf("%d is Zero\n", n);
+		printf("%d is negative\n", n);
 	return (0);
 }
