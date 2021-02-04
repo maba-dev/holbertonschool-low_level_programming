ffa44de383416d7b7f1008f6793b99727c0fc1aa
maba-dev <cherif.ba@hotmail.fr>
2021-02-04 15:20:50 +0000

    using two boucle while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+* main - entry poin
+* more headers goes there
+* betty style doc for function main goes there
+* Return: Return description
+*/
+
+int	main(void)
+{
+	char i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		putchar(i);
+		i++;
+	}
+	i = 'A';
+	while (i <= 'Z')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
