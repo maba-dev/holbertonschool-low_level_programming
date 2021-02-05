d3354f16a4b0c52e1fb0826abdd595135071f573
barifche <barifche@gmail.com>
2021-02-05 10:07:50 +0100

    correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,13 @@
 
 /**
  * main - Main function
- * Return: After printing, return 0
+ * Return: After printing, return 1
 */
 
 int	main(void)
 
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
 
