dfd8d9ce53243381a02a88f3c78775127807794e
maba-dev <cherif.ba@hotmail.fr>
2021-02-05 21:36:02 +0000

    the last

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+* main - entry poin
+* more headers goes there
+* betty style doc for function main goes there
+* Return: Return description
+*/
+
+int	main(void)
+
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 1;
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(32);
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(32);
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
