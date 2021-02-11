b4010f5b5aca9c02d5e9be3be756007570680e93
maba-dev <cherif.ba@hotmail.fr>
2021-02-11 20:35:21 +0000

    some modif of ex9 in changing the boucle while, i use for

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
 
 /**
-*main - print numbers 1 to 100
-*
-*Return: print to stdout numbers 1 to 100 except:
-*replace multiples of three print Fizz
-*replace multiples of five print Buzz
-*numbers which are multiples of both three and five print FizzBuzz
-*/
-
+ *main - print numbers 1 to 100
+ *
+ *Return: print to stdout numbers 1 to 100 except:
+ *replace multiples of three print Fizz
+ *replace multiples of five print Buzz
+ *numbers which are multiples of both three and five print FizzBuzz
+ */
 int	main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if ((i % 3) == 0 && (i % 5) == 0)
+		{
 			printf("FizzBuzz ");
-		else if (i %  3 == 0)
+		}
+		else if ((i % 3) == 0)
+		{
 			printf("Fizz ");
-		else if (i % 5 == 0)
+		}
+		else if ((i % 5) == 0)
+		{
 			printf("Buzz ");
+		}
 		else
+		{
 			printf("%d ", i);
+		}
 	}
 	printf("\n");
+
 	return (0);
 }
