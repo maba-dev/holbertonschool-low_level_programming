474b1b8a8e18a86fd9e5a761c0426811caba2ea3
maba-dev <cherif.ba@hotmail.fr>
2021-02-12 10:41:25 +0000

    my last test in ex9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -21,7 +21,7 @@ int main(void)
 		else if ((i % 3) == 0)
 			printf("Fizz ");
 		else if (i == 100)
-			printf("FizzBuzz");
+			printf("Buzz");
 		else if ((i % 5) == 0)
 			printf("Buzz ");
 		else
