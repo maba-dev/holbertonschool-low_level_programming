f718c1119f499952149e033aece413d26819aa74
maba-dev <cherif.ba@hotmail.fr>
2021-02-18 23:15:13 +0000

    update fe holberton.h file

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -10,10 +10,12 @@ void 	reverse_array(int *a, int n);
 
 char 	*string_toupper(char *str);
 
-char 	*cap_string(char *);
+char 	*cap_string(char *str);
 
-char 	*leet(char *);
+char 	*leet(char *str);
 
-char	*rot13(char *);
+char	*rot13(char *str);
 
 void	print_number(int n);
+
+int	_putchar(char c);
