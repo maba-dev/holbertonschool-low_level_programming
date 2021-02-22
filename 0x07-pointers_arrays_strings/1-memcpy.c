eaba37e2ab64cc879068ed5ddec3cd72a770f3cd
maba-dev <cherif.ba@hotmail.fr>
2021-02-22 11:56:14 +0000

    a function that copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* _memcpy - function that copies memory area.
+* @dest: dest positions
+* @src: source position
+* @n: size of bytes
+* Return: char
+*/
+
+char	*_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
