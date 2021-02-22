3e5c64bb7e09c762b65d04628e959a2a51b4ea43
maba-dev <cherif.ba@hotmail.fr>
2021-02-22 19:31:10 +0000

    a function that prints the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+* print_chessboard - function that prints a chessboard
+* @a: size of 2D array
+* Return: void
+*/
+
+void	print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
