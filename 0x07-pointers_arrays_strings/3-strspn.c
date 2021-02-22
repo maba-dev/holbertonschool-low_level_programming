df2f3982300155c25756f1a988111422bf33c4c8
maba-dev <cherif.ba@hotmail.fr>
2021-02-22 14:37:17 +0000

    a function that gets the length of a prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+ * _strspn - function that gets the length of a prefix substring.
+ * @s: initial segment
+ * @accept: bytes to check
+ * Return: unsigned int
+ */
+
+unsigned	int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	unsigned  int n;
+
+	i = 0;
+	n = 0;
+	if (accept[i] == 0)
+		return (0);
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i + j] == accept[j])
+				n++;
+			j++;
+		}
+		i++;
+	}
+	return (n);
+}
