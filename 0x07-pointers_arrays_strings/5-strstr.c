ab84b90dfa82e80979800d083f79880bf359967b
maba-dev <cherif.ba@hotmail.fr>
2021-02-22 19:03:30 +0000

    a function that locates a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stddef.h>
+
+/**
+* _strstr -  function that locates a substring.
+* @haystack: initial string
+* @needle: substring
+* Return: char
+*/
+
+char	*_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		while (needle[j] == haystack[i + j])
+		{
+			j++;
+			if (needle[j] == '\0')
+				return (&haystack[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
