3eca1e1190dd5034bc63da0f245e5a965555a87e
maba-dev <cherif.ba@hotmail.fr>
2021-02-24 22:33:17 +0000

    function palindrome

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,51 @@
+#include "holberton.h"
+
+/**
+* _strlen_recursion - fonction tha return the length of string
+* @s : string
+* Return: int
+*/
+int	_strlen_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	if (s[i] != '\0')
+	{
+		i = _strlen_recursion(s + 1);
+		i++;
+	}
+	return (i);
+}
+
+/**
+* palindrome - function that return 1 if a palindrome 0 else
+* @s : string
+* @j: int
+* @i: int
+* Return : int
+*/
+int	palindrome(char *s, int j, int i)
+{
+	i  = _strlen_recursion(s);
+	if (s[i - 1] != s[j])
+		return (0);
+	if (s[j] >= i)
+		return (1);
+	else
+		return (palindrome(s, j + 1, i - 1));
+}
+/**
+* is_palindrome - returns 1 if a string is a palindrome and 0 if not.
+* @s: string
+* Return: int
+*/
+int	is_palindrome(char *s)
+{
+	int i;
+
+	i = _strlen_recursion(s);
+	if (i == 0)
+		return (0);
+	return (palindrome(s, 0, i));
+}
