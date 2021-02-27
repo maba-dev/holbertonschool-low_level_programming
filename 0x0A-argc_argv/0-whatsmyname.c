0e778068c2afbea89d06b4b83d0fbdab811e5611
maba-dev <cherif.ba@hotmail.fr>
2021-02-27 10:38:03 +0000

    a program that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - entry point
+* @argc: count argement
+* @argv: parameter argument
+* Return: int
+*/
+
+int	main(int argc, char **argv)
+{
+	if (argc != 1)
+		return (0);
+	printf("%s\n", argv[0]);
+}
