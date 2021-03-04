2908bfc304063b0c6b931cc402f7d3127da07e15
maba-dev <cherif.ba@hotmail.fr>
2021-03-04 15:04:53 +0000

    correction

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,31 +15,29 @@ char	*string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int size2;
 	unsigned int i;
 	char *str;
-	char *nul;
 
 	i = 0;
 	size2 = 0;
 	size2 = 0;
-	nul = "";
 	if (s1 == NULL)
-		s1 = nul;
+		s1 = "";
 	while (s1[size1] != '\0')
 		size1++;
 	if (s2 == NULL)
-		s2 = nul;
+		s2 = "";
 	while (s2[size2] != '\0' && size2 < n)
 		size2++;
 	if (n >= size2)
 		n = size2;
-	str = malloc((size1 + n + 1) * sizeof(char));
+	str = malloc((size1 + n) * sizeof(char));
 	if (str == NULL)
 		return (NULL);
-	size2 = 0;
 	while (i < size1)
 	{
 		str[i] = s1[i];
 		i++;
 	}
+	size2 = 0;
 	while (size2 < n && s2[size2] != '\0')
 	{
 		str[i] = s2[size2];
