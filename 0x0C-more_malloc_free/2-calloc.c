d67677d130ce07192b8b1761179f4f37060ff7e5
maba-dev <cherif.ba@hotmail.fr>
2021-03-04 19:16:42 +0000

    the correctly

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	i = 0;
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ptr = malloc(nmemb * size);
+	ptr = malloc(nmemb * size * sizeof(*ptr));
 	if (ptr == NULL)
 		return (NULL);
 	while (i < nmemb)
