88e343341ba958d9f27128a27f2a92a74bf4cb26
maba-dev <cherif.ba@hotmail.fr>
2021-03-08 20:32:02 +0000

    add the dog.h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,5 +14,6 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog;
-void init_dog(struct dog *d, char *name, float age, char *owner);
+void	init_dog(struct dog *d, char *name, float age, char *owner);
+void	print_dog(struct dog *d);
 #endif
