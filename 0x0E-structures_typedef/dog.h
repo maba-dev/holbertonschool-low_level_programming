8d5f6c65003bced854564f7a5b0ba473a124a24c
maba-dev <cherif.ba@hotmail.fr>
2021-03-08 17:53:47 +0000

    a new type struct dog with the following elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog_H
+#define dog_H
+
+/**
+* struct dog - struture of a dog
+* @name: char
+* @age: float
+* @owner: char
+*/
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+#endif
