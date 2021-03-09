e8be2871575fe58057b4acd1688407f779b163a7
maba-dev <cherif.ba@hotmail.fr>
2021-03-09 17:43:49 +0000

    add retry

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -55,12 +55,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	new_dog->name = malloc(_strlen(name) + 1 * sizeof(char));
 	if (new_dog->name == NULL)
+	{
+		free(new_dog->name);
 		return (NULL);
+	}
 	new_dog->name = _strcpy(new_dog->name, name);
 	new_dog->age = age;
 	new_dog->owner = malloc(_strlen(owner) + 1 * sizeof(char));
 	if (new_dog->owner == NULL)
+	{
+		free(new_dog->owner);
+		free(new_dog->name);
 		return (NULL);
+	}
 	new_dog->owner = _strcpy(new_dog->owner, owner);
 	return (new_dog);
 }
