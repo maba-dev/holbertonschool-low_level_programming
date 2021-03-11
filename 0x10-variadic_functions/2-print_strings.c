c185236cb9c73b03432971107948ffefc24c4677
maba-dev <cherif.ba@hotmail.fr>
2021-03-11 17:28:03 +0000

    retry

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,8 +17,10 @@ void    print_strings(const char *separator, const unsigned int n, ...)
 
 	i = 0;
 	va_start(str, n);
-	while (i < n && str != '\0')
+	while (i < n)
 	{
+		if (str == NULL)
+			printf("nil");
 		printf("%s", va_arg(str, char *));
 		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
