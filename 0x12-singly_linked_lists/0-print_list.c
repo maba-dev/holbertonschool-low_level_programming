8699b5b2175f33e33b8da23fcf211f626cbf39b1
maba-dev <cherif.ba@hotmail.fr>
2021-03-18 17:47:12 +0000

    a function that prints all the elements of a list_t list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+*print_list -  a function that prints all the elements of a list_t list
+*@h: linked list to print
+*Return: message to stdout
+*/
+
+size_t print_list(const list_t *h)
+{
+	int i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nill)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+return (i);
+}
