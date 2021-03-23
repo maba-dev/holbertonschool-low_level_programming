ff3e2a289828ba08ce9988726cc66446ac173d3f
maba-dev <cherif.ba@hotmail.fr>
2021-03-23 21:54:31 +0000

    a function that inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,7 +15,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	actual_node = *head;
-	i = 0;
+	i = 1;
 	while (i < idx && actual_node)
 	{
 		actual_node = actual_node->next;
