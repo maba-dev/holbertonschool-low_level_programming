b23d6976b65973239ef16bbd0dd50b7e40058015
maba-dev <cherif.ba@hotmail.fr>
2021-03-23 18:18:08 +0000

    a function that returns the nth node of a listint_t linked list

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+*get_nodeint_at_index - a function that returns the nth node of a list
+*@head: head of element
+*@index: index of nth
+*
+*Return: nth of node
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < index)
+	{
+		head = head->next;
+		i++;
+	}
+	if (head)
+		return (head);
+	return (NULL);
+}
