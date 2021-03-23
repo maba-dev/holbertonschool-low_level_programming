02483bd11a04191632f497b21a304610f85fb269
maba-dev <cherif.ba@hotmail.fr>
2021-03-23 16:16:45 +0000

    a function that frees a listint_t list.

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+*free_listint2 - a function that frees a listint_t list.
+*@head: head of element
+*/
+void free_listint2(listint_t **head)
+{
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		//*head = (*head)->next;
+		free(*head);
+		*head = (*head)->next;
+	}
+}
