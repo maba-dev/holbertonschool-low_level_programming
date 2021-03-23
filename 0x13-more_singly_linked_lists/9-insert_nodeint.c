c2d300c5d6bb2479908c6e098199232a582066e2
maba-dev <cherif.ba@hotmail.fr>
2021-03-23 21:35:16 +0000

    a function that inserts a new node at a given position

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+*insert_nodeint_at_index -  a function that inserts a new node
+*@head: head of element
+*@idx: index of element
+*@n: value of element
+*Return: new node
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+
+	listint_t *actual_node;
+	listint_t *new_node;
+	unsigned int i;
+
+	actual_node = *head;
+	i = 0;
+	while (i < idx && actual_node)
+	{
+		actual_node = actual_node->next;
+		i++;
+	}
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	if (idx == 0)
+	{
+		*head = new_node;
+		new_node->next = actual_node;
+	}
+	else if (actual_node->next)
+	{
+		new_node->next = actual_node->next;
+		actual_node->next = new_node;
+	}
+	else
+	{
+		new_node->next = NULL;
+		actual_node->next = new_node;
+	}
+	return (new_node);
+}
