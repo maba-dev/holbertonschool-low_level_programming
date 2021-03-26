582e06737ff3ff2bc83bfd51606e9c10907cec5d
maba-dev <cherif.ba@hotmail.fr>
2021-03-26 22:41:36 +0000

    a function that checks the endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+*get_endianness - a function that checks the endianness.
+*
+*Return: 0 if big endian, 1 if little endian
+*/
+
+int get_endianness(void)
+{
+	char i;
+	char *p;
+
+	num = '1';
+	p = &i;
+	if (*p == 0)
+		return (0);
+	else
+		return (1);
+}
