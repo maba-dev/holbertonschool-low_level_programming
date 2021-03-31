ff32cf439b56ff94c3c05489be941ba1f37004ae
maba-dev <cherif.ba@hotmail.fr>
2021-03-31 12:25:18 +0000

    retry

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	while (text_content[len] != '\0')
+	while (text_content && text_content[len])
 		len++;
 	if (text_content == NULL)
 		text_content = "";
