cf735db77158d1e49538364e714069245ef52026
maba-dev <cherif.ba@hotmail.fr>
2021-04-14 15:20:48 +0000

    betty

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* main - entry point
-* @argc: count argument
-* @argv: parameter argument
-* Return: int
+* _isdigit - the digit function
+* @c: char c
+* Return: 0 or 1
 */
 int     _isdigit(char c)
 {
-        if (c >= '0' && c <= '9')
-                return (1);
-        else
-                return (0);
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 }
-
+/**
+* _isdigit_str - the digit function string
+* @s: char s
+* Return: 0 or 1
+*/
 int	_isdigit_str(char *s)
 {
 	int i;
@@ -27,8 +30,13 @@ int	_isdigit_str(char *s)
 	}
 	return (1);
 }
-
-int	main(int argc, char **argv)
+/**
+*main - entry point
+* @argc: count argument
+* @argv: parameter argument
+* Return: int
+*/
+int main(int argc, char **argv)
 {
 	int i;
 	int result;
@@ -43,15 +51,13 @@ int	main(int argc, char **argv)
 	while (argc > i)
 	{
 		if (_isdigit_str(argv[i]) == 0)
-       		{
-                	printf("Error\n");
-       			return (1);
+		{
+			printf("Error\n");
+			return (1);
 		}
 		i++;
-        }
-//	printf("%d\n", i);
+	}
 	i = i - 1;
-
 	while (i > 0)
 	{
 		result += atoi(argv[i]);
