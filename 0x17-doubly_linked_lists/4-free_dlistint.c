372ed2b48b6f870ea43ec18bde209250973349ba
maba-dev <cherif.ba@hotmail.fr>
2022-01-06 21:37:21 +0100

    correct betty

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,15 +1,14 @@
 #include "lists.h"
 /**
- * 
- * 
- * 
+ * free_dlistint - a function that frees a dlistint_t list.
+ * @head: head of node
  */
 
-void free_dlistint(dlistint_t *head)
+void	free_dlistint(dlistint_t *head)
 {
-    if (head == NULL)
-        return;
-    if (head->next != NULL)
-        free_dlistint(head->next);
-    free (head);
+	if (head == NULL)
+		return;
+	if (head->next != NULL)
+		free_dlistint(head->next);
+	free(head);
 }
