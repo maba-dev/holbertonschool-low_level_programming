c70ec418d835bfb6d3ef992188f2fd7f874df0b5
maba-dev <cherif.ba@hotmail.fr>
2022-01-06 22:52:14 +0100

    task 6

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,22 +1,21 @@
 #include "lists.h"
 /**
- * 
- * 
- * 
- * 
+ *sum_dlistint -  a function that returns the sum of all the data (n)
+ *@head: head node of element
+ *Return: sum value of node
  */
 
-int sum_dlistint(dlistint_t *head)
+int	sum_dlistint(dlistint_t *head)
 {
-    int result;
+	int result;
 
-    result = 0;
-    if (head == NULL)
-        return 0;
-    while(head != NULL)
-    {
-        result += head->n;
-        head = head->next;
-    }
-    return result;
+	result = 0;
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		result += head->n;
+		head = head->next;
+	}
+	return (result);
 }
