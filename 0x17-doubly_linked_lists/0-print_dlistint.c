f69d9688e5dd87fa1c89d08fddb6e9993b6ee294
maba-dev <cherif.ba@hotmail.fr>
2022-01-06 16:02:27 +0100

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 /**
- * size_t print_dlistint: a function that prints all the elements of a dlistint
+ * print_dlistint - a function that prints all the elements of a dlistint
  * @h: head of element of list
  * Return: the number of nodes
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t	print_dlistint(const dlistint_t *h)
 {
 	int i;
 
