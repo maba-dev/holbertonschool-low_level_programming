d995fc14eb860862dae5a1f1f37da4d63755b9a2
maba-dev <cherif.ba@hotmail.fr>
2022-01-06 21:07:00 +0100

    task3 correct betty

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,31 +1,32 @@
 #include "lists.h"
 /**
-*
-*
-* 
+*add_dnodeint_end - a function that adds a new node at the end of a list.
+*@head: head node in list
+* @n: element to add
+* Return:  the address of the new element, or NULL if it failed
 */
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node;
-    dlistint_t *actual_node = NULL;
+	dlistint_t *new_node;
+	dlistint_t *actual_node = NULL;
 
-    if (head == NULL)
-        return NULL;
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return NULL;
-    new_node->n = n;
-    new_node->next = NULL;
-    actual_node = *head;
-    while (actual_node && actual_node->next != NULL)
-        actual_node = actual_node->next;
-    if (actual_node != NULL)
-    {
-        actual_node->next = new_node;
-        new_node->prev = actual_node;
-    }
-    else
-        *head = new_node;
-    return new_node;
+	if (head == NULL)
+		return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	actual_node = *head;
+	while (actual_node && actual_node->next != NULL)
+		actual_node = actual_node->next;
+	if (actual_node != NULL)
+	{
+		actual_node->next = new_node;
+		new_node->prev = actual_node;
+	}
+	else
+		*head = new_node;
+	return (new_node);
 }
