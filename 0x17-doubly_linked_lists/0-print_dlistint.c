9572152d4272735965e33bd29c8f31df1737e7b2
maba-dev <cherif.ba@hotmail.fr>
2022-01-06 15:55:17 +0100

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * size_t print_dlistint: a function that prints all the elements of a dlistint
+ * @h: head of element of list
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i;
+
+	i = 0;
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
