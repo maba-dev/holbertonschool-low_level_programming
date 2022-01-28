658c07497df9c16de16f6ffe0e16e073e7a73282
maba-dev <cherif.ba@hotmail.fr>
2022-01-28 10:00:11 +0100

    task 0 retry

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,7 +1,8 @@
 #include "hash_tables.h"
 /**
- * 
- * 
+ * hash_table_create -  a function that creates a hash table
+ * @size: the size of the hash table
+ * Return: a pointer to the newly created hash table
  */
 
 
@@ -9,16 +10,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_table;
 
+
 	new_hash_table = malloc(sizeof(hash_table_t));
 	if (new_hash_table == NULL)
-		return NULL;
+		return (NULL);
 	new_hash_table->size = size;
-	new_hash_table-> array = malloc(sizeof (hash_node_t) * size);
+	new_hash_table->array = malloc(sizeof(hash_node_t) * size);
 	if (new_hash_table->array == NULL)
 	{
-		free (new_hash_table);
-		return NULL;
+		free(new_hash_table);
+		return (NULL);
 	}
 	return (new_hash_table);
-
 }
