15534fb2393264d71078f1b0df2f39cc7e20dedb
maba-dev <cherif.ba@hotmail.fr>
2022-01-28 15:11:55 +0100

    task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ *  
+ * 
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{   unsigned int i;
+
+    i = 0;
+    if (!ht)
+        return;
+    printf("{");
+    while(i < ht->size)
+    {
+        while(ht->array[i])
+        {
+            if (!(ht->array[i]->next))
+                printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+            else
+                printf("'%s': '%s', ", ht->array[i]->key, ht->array[i]->value);
+            ht->array[i] = ht->array[i]->next;
+        }
+        i++;
+    }
+    printf("}\n");
+}
