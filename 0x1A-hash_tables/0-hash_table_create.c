43f2fbaed69fa7654bbd53c69b9ca2ff41b300d4
maba-dev <cherif.ba@hotmail.fr>
2022-01-28 10:13:14 +0100

    task 0 retry

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,7 +12,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 
 	new_hash_table = malloc(sizeof(hash_table_t));
-	if (new_hash_table == NULL)
+	if (new_hash_table == NULL || size == 0)
 		return (NULL);
 	new_hash_table->size = size;
 	new_hash_table->array = malloc(sizeof(hash_node_t) * size);
