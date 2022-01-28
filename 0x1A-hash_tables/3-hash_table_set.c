e24f47b29b1511f7f6ed557c746dbf04ab86eef5
maba-dev <cherif.ba@hotmail.fr>
2022-01-28 13:13:59 +0100

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,32 +1,39 @@
 #include "hash_tables.h"
 
 /**
- * 
- * 
+ * hash_table_set - a function that adds an element to the hash table.
+ * @ht: the hash table
+ * @key: the key of the element
+ * @value: the value of the element
+ * Return: 1 if succesful else 0
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    hash_node_t *node = NULL;
-    unsigned long int idx;
+	hash_node_t *node = NULL;
+	unsigned long int idx;
 
-    if (!ht || !key || !value)
-        return 0;
-    
-    node = malloc(sizeof(hash_node_t));
-    idx = key_index((const unsigned char *)key, ht->size);
-    if (!node)
-        return 0;
+	if (!ht || !key || !value)
+		return (0);
 
-    while(ht->array[idx])
-    {
-        if (ht->array[idx]->key == key)
-        {
-            ht->array[idx]->value = strdup(value);
-            return(1);
-        }
-        ht->array[idx] = ht->array[idx]->next;
+	node = malloc(sizeof(hash_node_t));
+	idx = key_index((const unsigned char *)key, ht->size);
+	if (!node)
+		return (0);
 
-    }
-    return(1);
+	while (ht->array[idx])
+	{
+		if (ht->array[idx]->key == key)
+		{
+			ht->array[idx]->value = strdup(value);
+			return (1);
+		}
+		ht->array[idx] = ht->array[idx]->next;
+
+	}
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = ht->array[idx];
+	ht->array[idx] = node;
+	return (1);
 }
