d1633477794d4aeb1d1ae3cc6f4423d2bc94d610
maba-dev <cherif.ba@hotmail.fr>
2022-01-28 18:17:01 +0100

    task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,19 +1,22 @@
 #include "hash_tables.h"
 /**
- *  
- * 
+ * hash_table_delete - a function that deletes a hash table.
+ * @ht: the hash table
+ * Return: None
  */
 
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i;
-    i = 0;
+	unsigned long int i;
+
+
+	i = 0;
 	while (i < ht->size)
-    {
+	{
 		if (ht->array[i])
 			free(ht->array[i]);
-        i++;
-    }
+		i++;
+	}
 	free(ht->array);
 	free(ht);
 }
