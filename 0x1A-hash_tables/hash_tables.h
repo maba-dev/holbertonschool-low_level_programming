039bba8794ae9b05b19ab4ad453f8490d3f140e9
maba-dev <cherif.ba@hotmail.fr>
2022-01-28 10:52:13 +0100

    retry

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,9 +1,5 @@
-#ifndef MABA
-#define MABA
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -34,18 +30,17 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
 hash_table_t *hash_table_create(unsigned long int size);
-
 unsigned long int hash_djb2(const unsigned char *str);
-
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-
+hash_node_t *add_node(hash_node_t *array, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
-
 void hash_table_print(const hash_table_t *ht);
-
 void hash_table_delete(hash_table_t *ht);
 
-#endif
+#endif /* HASH_TABLES_H */
