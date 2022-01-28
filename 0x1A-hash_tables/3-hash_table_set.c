3147845d521025ed783498f166ab98d6dfbdeea0
maba-dev <cherif.ba@hotmail.fr>
2022-01-28 13:19:19 +0100

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	while (ht->array[idx])
 	{
-		if (ht->array[idx]->key == key)
+		if (strcmp(ht->array[idx]->key, key) == 0)
 		{
 			ht->array[idx]->value = strdup(value);
 			return (1);
