fd3b50ea18fe18b3e4a660854b013f146d8a0edc
maba-dev <cherif.ba@hotmail.fr>
2022-03-17 17:02:22 +0100

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-"""
-    a function def island_perimeter(grid): that returns
-    the perimeter of the island described in grid:
- """
-
-
 def island_perimeter(grid):
     perimeter = 0
     for i in range(0, len(grid)):
