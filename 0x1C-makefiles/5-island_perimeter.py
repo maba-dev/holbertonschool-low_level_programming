13e036d3a53481fb3004a4a013d0eca8bf139104
maba-dev <cherif.ba@hotmail.fr>
2022-03-17 17:01:34 +0100

    task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,17 +4,15 @@
     the perimeter of the island described in grid:
  """
 
- 
+
 def island_perimeter(grid):
     perimeter = 0
-        
     for i in range(0, len(grid)):
         for j in range(0, len(grid[0])):
-                
             if grid[i][j] == 1:
-                perimeter +=4
-            if grid[i][j]==1 and j>=1 and grid[i][j-1]==1:
-                perimeter -=2    
-            if grid[i][j]==1 and i>=1 and grid[i-1][j]==1:
-                perimeter -=2                    
+                perimeter += 4
+            if grid[i][j] == 1 and j >= 1 and grid[i][j-1] == 1:
+                perimeter -= 2
+            if grid[i][j] == 1 and i >= 1 and grid[i-1][j] == 1:
+                perimeter -= 2
     return perimeter
