b9582ab57d7922427d11db6dc7f07ea238958285
maba-dev <cherif.ba@hotmail.fr>
2022-05-05 14:36:06 +0000

    task 102

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+/**
+*interpolation_search - searches for a value in an array of integers
+*@array: array of elemnt
+*@size: number of element in array
+*@value: value searched
+*Return: index of value
+*/
+
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t mid;
+	size_t bottom = 0;
+
+	while (bottom < size && value <= array[size - 1])
+	{
+		mid = bottom + (((double)(size - 1 - bottom) /
+		 (array[size - 1] - array[bottom])) * (value - array[bottom]));
+		printf("Value checked array[%lu] = [%d]\n", mid, array[mid]);
+		if (value == array[mid])
+			return (mid);
+		if (value < array[mid])
+			size = mid - 1;
+		else
+			bottom = mid + 1;
+	}
+	mid = bottom + (((double)(size - 1 - bottom) /
+	 (array[size - 1] - array[bottom])) * (value - array[bottom]));
+	printf("Value checked array[%lu] is out of range\n", mid);
+	return (-1);
+}
