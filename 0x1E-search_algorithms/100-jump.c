caae44b0a5d86b91de83769c615190c005ab6858
maba-dev <cherif.ba@hotmail.fr>
2022-05-05 11:49:40 +0000

    task 100

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,31 @@
+#include "search_algos.h"
+/**
+*jump_search - searches for a value in an array of integers
+*@array: array of elemnt
+*@size: number of element in array
+*@value: value searched
+*Return: index of value
+*/
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t step = 0;
+	int i = 0;
+	size_t x;
+
+	while (array[step] < value && step < size)
+	{
+		printf("Value checked array[%lu] = [%d]\n", step, array[step]);
+		i = step;
+		step += sqrt(size);
+	}
+
+	printf("Value found between indexes [%d] and [%lu]\n", i, step);
+	for (x = i; x <= step && x < size; x++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", x, array[x]);
+		if (array[x] == value)
+			return (x);
+	}
+	return (-1);
+}
